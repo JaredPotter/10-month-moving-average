99e1d9745f5b282245a89522fc9b4c41af7186a0
Jared Potter <jaredpotter1@gmail.com>
2020-02-09 19:57:59 -0700

    Added refactor of 'what-if' project into new RESTful endpoint. Removed old 'what-if' project. Moving Data. Added capital gains tax json.

--- functions/index.js
+++ functions/index.js
@@ -107,6 +107,9 @@ async function fetchData(symbol, lastUpdated, currentData) {
 
     // const nowUnix = 946684800;
 
+// Example URL
+// https://query1.finance.yahoo.com/v8/finance/chart/VOO?symbol=VOO&period1=729129600&period2=1579736788&interval=1d&events=div;
+// https://query1.finance.yahoo.com/v8/finance/chart/VFINX?symbol=VFINX&period1=729129600&period2=1579736788&interval=1d&events=div;    
     const url = `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?symbol=${symbol}&period1=${lastUpdated}&period2=${nowUnix}&interval=1d&events=div`;
     const response = await axios.get(url);
 
@@ -391,12 +394,13 @@ exports.tenMonthMovingAverages = functions.https.onRequest(async (req, res) => {
 
     switch(method) {
         case 'GET':
-            const query = {};
+            // TODO: add database filtering at the right time.
+            // const query = {};
 
-            if(start && end) {
-                query.start = Number(start);
-                query.end = Number(end);
-            }
+            // if(start && end) {
+            //     query.start = Number(start);
+            //     query.end = Number(end);
+            // }
 
             const symbol = await mongoDbService.findOne({ _id: id });
             const tenMonthMovingAverages = symbol.tenMonthMovingAverages;
@@ -444,6 +448,330 @@ exports.latestTenMonthMovingAverage = functions.https.onRequest(async (req, res)
     // });    
 });
 
+exports.whatIfCalculate = functions.https.onRequest(async (req, res) => {
+    const method = req.method;
+
+    switch(method) {
+        case 'POST':    
+            const parameters = {
+                currentBalance: req.body.currentBalance,
+                start: req.body.start,
+                end: req.body.end,
+                symbol: req.body.symbol,
+                yearsWorked: req.body.yearsWorked,
+                isInMarket: req.body.isInMarket,
+            };
+
+            const results = whatIfCalculateHandler(parameters);
+            res.send(results);
+            return;
+    }
+        
+    res.send();
+});
+    
+async function whatIfCalculateHandler(params) {
+    let method = '';
+    // let results = null;
+
+    const parameters = { ...params };
+    // Calculate (Simulated) Worst Case - TODO: Later
+
+    // Calculate "Traditional" Case
+    method = 'traditional';
+    parameters.method = method;
+    const traditionalResults = await whatIfCalculate(parameters);
+    
+    // Calculate 10 Month Moving Average Case
+    method = 'tenMonthMovingAverage';
+    parameters.method = method;
+    const tenMonthMovingAverageResults = await whatIfCalculate(parameters);
+
+    const results = {
+        traditional: traditionalResults,
+        tenMonthMovingAverage: tenMonthMovingAverageResults,
+    };
+    debugger; 
+    return results;
+}
+
+async function whatIfCalculate(params) {
+    let taxesOwed = 0;
+    let feesPaid = 0;
+    let dividendTotalReceived = 0;
+    let previousPrice = null;
+    let isInMarket = params.isInMarket;
+    let marketMoney = 0;
+    let cashMoney = 0;
+    const tradingDaysPerYear = 252;
+    let dailyExpenseRatio = 0;
+    const threshold = 0; // TODO: hookup as a parameter.
+    
+    const startTimestamp = Number(params.start);
+    const endTimestamp = Number(params.end);
+    
+    // debugger;
+    
+    // Initializing money.
+    if(isInMarket) {
+        marketMoney = params.currentBalance;
+    }
+    else {        
+        cashMoney = params.currentBalance;
+    }
+    
+    let lastBuy = null;
+    
+    // Lookup Symbol to get expense ratio.
+    const symbols = await mongoDbService.find({ _id: params.symbol });
+    
+    let expenseRatio = 0;
+    let tenMonthMovingAverages = [];
+    let prices = [];
+    let dividends = {};
+    
+    if(symbols.length > 0) {
+        const symbol = symbols[0];
+        expenseRatio = Number(symbol.expenseRatio.toString());
+        dailyExpenseRatio = expenseRatio / tradingDaysPerYear;
+        dividends = symbol.dividends;
+        prices = symbol.prices.filter((price) => {
+            if(price.timestamp >= startTimestamp && price.timestamp <= endTimestamp) {
+                return true;
+            }
+        });
+
+        if(params.method === 'tenMonthMovingAverage') { 
+            // Fetch precalculated values.
+            tenMonthMovingAverages = symbol.tenMonthMovingAverages.averages.filter((average) => {
+                if(Number(average.lastUpdated) >= startTimestamp && Number(average.lastUpdated) <= endTimestamp) {
+                    return true;
+                }
+            });
+        }
+    }
+
+    if(params.method === 'traditional') {
+        for(let dailyClosingPrice of prices) {
+            const timestamp = dailyClosingPrice.timestamp;
+
+            // 1. Calculate Change in Price
+            if(previousPrice) {
+                marketMoney = calculateDailyChangeInValue(previousPrice, dailyClosingPrice, marketMoney);
+            }
+
+            previousPrice = dailyClosingPrice.price;            
+            
+            // 2. If Dividend Day, Calculate Dividend
+            if(dividends[timestamp]) {
+                let dividendAmount = dividends[timestamp].amount;
+                const shareCount = marketMoney / dailyClosingPrice.price;
+                const earnedDividendAmount = shareCount * dividendAmount;
+
+                dividendTotalReceived += earnedDividendAmount;
+
+                marketMoney += earnedDividendAmount
+            }
+
+            // 3. Calculate Expense Ratio Fees
+            const dailyFees = marketMoney * dailyExpenseRatio;
+
+            feesPaid += dailyFees;
+            marketMoney -= dailyFees;
+        }
+    }
+    else if(params.method === 'tenMonthMovingAverage') { 
+        // Fetch precalculated values.
+        let nextBuySellTimestamp;
+        let nextBuySellIndex = 1;
+
+        if(isInMarket) {
+            lastBuy = prices[0];
+        }
+
+        if(tenMonthMovingAverages.length > 0) {
+            nextBuySellTimestamp = Number(tenMonthMovingAverages[0].lastUpdated);
+        }
+
+        for(let dailyClosingPrice of prices) {
+            // debugger;
+            const timestamp = dailyClosingPrice.timestamp;
+            nextBuySellTimestamp = Number(tenMonthMovingAverages[nextBuySellIndex].lastUpdated);
+            let isBuyOrSellDay = timestamp === nextBuySellTimestamp ? true : false;
+            
+            if(isInMarket) {
+                // 1. Calculate Change in Price
+                if(previousPrice) {
+                    marketMoney = calculateDailyChangeInValue(previousPrice, dailyClosingPrice, marketMoney);
+                }
+                
+                previousPrice = dailyClosingPrice.price;
+
+                // 2. If Dividend Day, Calculate Dividend
+                if(dividends[timestamp]) {
+                    let dividendAmount = dividends[timestamp].amount;
+                    const shareCount = marketMoney / dailyClosingPrice.price;
+                    const earnedDividendAmount = shareCount * dividendAmount;
+
+                    dividendTotalReceived += earnedDividendAmount;
+
+                    marketMoney += earnedDividendAmount
+                }
+
+                // 3. Calculate Expense Ratio Fees
+                const dailyFees = marketMoney * dailyExpenseRatio;
+
+                feesPaid += dailyFees;
+                marketMoney -= dailyFees;                
+                
+                
+                if(isBuyOrSellDay) {
+                    const average = tenMonthMovingAverages[nextBuySellIndex];
+                    nextBuySellIndex++;
+
+                    if(average.average > average.closingDayPrice) {
+                        const withinThreshold = isWithinThreshold(average.closingDayPrice, average.average, threshold);
+
+                        if(!withinThreshold) {
+                            // SELL
+                            const buyPrice = lastBuy.price;
+                            const sellPrice = dailyClosingPrice.price;
+                            const net = (sellPrice - buyPrice) / buyPrice;
+                            // Net is used for tracking net gains / losses to calculate taxes.
+                            // TODO: Calculate taxes.
+                            lastBuy = null;
+                            cashMoney += marketMoney
+                            marketMoney = 0;
+                            // debugger;
+                            isInMarket = false;
+                        }
+                    }
+                }
+            }
+            else {
+                // Not in the Market
+
+                // 1. Determine if I should buy.
+                if(isBuyOrSellDay) {
+                    const average = tenMonthMovingAverages[nextBuySellIndex];
+                    nextBuySellIndex++;
+
+                    if(average.average < average.closingDayPrice) {
+                        const withinThreshold = isWithinThreshold(average.average, average.closingDayPrice, threshold);
+    
+                        if(!withinThreshold) {
+                            // BUY
+                            lastBuy = dailyClosingPrice;
+                            marketMoney += cashMoney;
+                            cashMoney = 0;
+                            // debugger;
+                            isInMarket = true;
+                        }
+                    }
+                }                
+            }
+        }
+    }
+
+    const results = {
+        taxesOwed,
+        feesPaid,
+        newBalance: marketMoney + cashMoney,
+        dividendTotalReceived
+    };
+
+    return results;
+};
+
+function calculateDailyChangeInValue(previousDayPrice, currentDay, marketMoney) {
+    let gainLossAmount = 0;
+    let newMarketMoney = marketMoney;
+
+    // Calculate Gains / Losses
+    if(previousDayPrice > currentDay.price) {
+        // loss
+        const loss = 1 - (currentDay.price / previousDayPrice);
+        gainLossAmount = newMarketMoney * loss;
+        newMarketMoney -= gainLossAmount;
+    }
+    else if(currentDay.price > previousDayPrice) {
+        // gain
+        const gain = 1 - (previousDayPrice / currentDay.price);
+        gainLossAmount = newMarketMoney * gain;
+        newMarketMoney += gainLossAmount;
+    }
+
+    return newMarketMoney;
+}
+
+function isWithinThreshold(smallerValue, largerValue, threshold) {
+    const percentDifference = 1 - (smallerValue / largerValue);
+
+    if((threshold / 100) > percentDifference) {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * 
+ * @param {*} amount The full amount in USD of dividend reward
+ * @param {*} salary 
+ * @param {*} year 
+ * @param {*} term 
+ */
+function calculateTaxForDividend(amount, salary, year, term) {
+    const brackets = taxes[year].brackets;
+
+    let taxRate;
+
+    for(let bracket of brackets) {
+
+        // Top Income Bracket
+        // if(!bracket.maxBracketValue) {
+        //     if(term === 'short') {
+        //         taxRate = bracket.shortTermRate / 100;
+        //     } 
+        //     else if(term === 'long') {
+        //         taxRate = bracket.longTermRate / 100;
+        //     }
+
+        //     return amount * taxRate
+        // }
+        // else {
+        //     if(bracket.minBracketValue <= salary && salary <= bracket.maxBracketValue) {
+    
+        //     }
+
+        // }
+    }
+    debugger;
+}
+
+const whatIfParameters = {
+    currentBalance: 200000,
+    start: 1183334400, // 07/02/2007
+    end: 1362182399, // 03/01/2013
+    symbol: 'SPY',
+    yearsWorked: 30,
+    isInMarket: true,
+};
+// const whatIfParameters = {
+//     currentBalance: 200000,
+//     start: 1046649600, // 03/03/2003
+//     end: 1191196800, // 10/01/2007
+//     symbol: 'SPY',
+//     yearsWorked: 30,
+//     isInMarket: true,
+// };
+
+(async () =>  {
+    const results = await whatIfCalculateHandler(whatIfParameters);
+
+    debugger;
+})();
+
 // fetchDailyData();
 
 // calculateTenMonthMovingAverage(0, 'SPY');
@@ -453,3 +781,6 @@ exports.latestTenMonthMovingAverage = functions.https.onRequest(async (req, res)
 // calculateTenMonthMovingAverage(753148800, 'SPY'); // 1993 / 11 / 13 (sat)
 
 // console.log('DEPLOYMENT SUCCESSFUL!');
+
+// 154.30 / 70.60 = 250000 / x
+// 
